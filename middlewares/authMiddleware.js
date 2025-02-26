3d9ae055996b8b57a8cb6b1699b036a0a4c3e523
Syed <syedmu729@gmail.com>
2025-02-26 18:00:35 +0500

    Just normal updates

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,17 +1,9 @@
 const { verifyToken } = require('../utils/jwt');
 
-const authMiddleware = (options = { tokenSource: 'header' }) => (req, res, next) => {
-    let token;
-
-    if (options.tokenSource === 'header') {
-        token = req.headers.authorization?.split(' ')[1];
-    } else if (options.tokenSource === 'cookie') {
-        token = req.cookies?.token;
-    }
-
-    if (!token) {
-        return res.status(401).json({ message: 'Authorization required' });
-    }
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    
+    if (!token) return res.status(401).json({ message: 'Authorization required' });
 
     try {
         const decoded = verifyToken(token);
@@ -20,7 +12,6 @@ const authMiddleware = (options = { tokenSource: 'header' }) => (req, res, next)
             return res.status(403).json({ message: 'Invalid or malformed token' });
         }
         req.user = { id: decoded.user.id, role_id: decoded.user.role_id };
-        console.log(req.user);
         next();
     } catch (error) {
         console.log(error.message)
